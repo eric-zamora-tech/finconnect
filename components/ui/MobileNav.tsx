00d5c1c3855dea9d90a5f04253ca17b126d32d76
eric-zamora-tech <eric.zamora.in@gmail.com>
2024-08-15 12:23:36 -0500

    Implement 3 column layout with mobile responsiveness

---
+++ components/ui/MobileNav.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import React from 'react'
+import Image from "next/image"
+
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet"
+import Link from 'next/link'
+import { sidebarLinks } from '@/constants'
+import { usePathname } from 'next/navigation'
+import { cn } from '@/lib/utils'
+
+const MobileNav = ({ user }: MobileNavProps) => {
+    const pathname = usePathname();
+
+    return (
+        <section className="w-full max-w-[264px]">
+            <Sheet>
+                <SheetTrigger><Image width={30} height={30} alt="menu" className="cursor-pointer" src="/icons/hamburger.svg" /></SheetTrigger>
+                <SheetContent side="left" className="border-none bg-white">
+                    <Link href="/"
+                        className="flex cursor-pointer items-center gap-1 px-4">
+                        <Image alt="FinConnect Logo" src="/icons/logo.svg" width={34} height={34} />
+                        <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">FinConnect</h1>
+                    </Link>
+                    <div className="mobilenav-sheet">
+                        <SheetClose asChild>
+                            <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+                                {sidebarLinks.map((item) => {
+                                    const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+                                    return (
+                                        <SheetClose asChild key={item.route}>
+                                            <Link href={item.route} key={item.label} className={cn('mobilenav-sheet_close w-full', {
+                                                "bg-bank-gradient": isActive
+                                            })}>
+                                                <Image src={item.imgURL} alt={item.label} width={20} height={20} className={cn({
+                                                    'brightness-[3] invert-0': isActive
+                                                })} />
+                                                <p className={cn('text-16 font-semibold text-black-2', { '!text-white': isActive })}>{item.label}</p>
+                                            </Link>
+                                        </SheetClose>
+                                    )
+                                })}
+
+                                USER
+                            </nav>
+                        </SheetClose>
+
+                        FOOTER
+                    </div>
+                </SheetContent>
+            </Sheet>
+        </section>
+    )
+}
+
+export default MobileNav
